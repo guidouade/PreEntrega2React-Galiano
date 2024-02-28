950004d0aa6bb28ab8a1f309f11754cc52f71f63
Guido Galiano <111579532+guidouade@users.noreply.github.com>
2024-02-27 22:39:20 -0300

    productsMock.js

--- src/productsMock.js
+++ src/productsMock.js
@@ -2,33 +2,33 @@ export let products = [
     {
         id: 1,
         title: "Rebelión en la Granja",
-        precio: 100,
+        precio: 3500,
         autor: "George Orwell",
         img: "https://images.cdn1.buscalibre.com/fit-in/360x360/68/e0/68e0aac2ed0bfe4c39e0cf16663a5918.jpg",
-        category: "Internacional"
+        category: "internacional"
     },
     {
         id: 2,
         title: "El Túnel",
-        precio: 100,
+        precio: 2000,
         autor: "Ernesto Sábato",
-        category: "Nacional",
+        category: "nacional",
         img: "https://images.cdn3.buscalibre.com/fit-in/360x360/b5/64/b564d07ef89c10880e519e687f9d51e4.jpg"
     },
     {
         id: 3,
         title: "Puro Fútbol",
-        precio: 100,
+        precio: 2500,
         autor: "Roberto Fontanarrosa",
-        category: "Nacional",
+        category: "nacional",
         img: "https://images.cdn3.buscalibre.com/fit-in/360x360/83/b4/83b4ff3eb5adfe2a55df65db14abb2fd.jpg"
     },
     {
         id: 4,
         title: "Cien Años de Soledad",
-        precio: 100,
+        precio: 5000,
         autor: "Gabriel García Marquez",
-        category: "Internacional",
+        category: "internacional",
         img: "https://images.cdn3.buscalibre.com/fit-in/360x360/61/8d/618d227e8967274cd9589a549adff52d.jpg"
     }
 ];
